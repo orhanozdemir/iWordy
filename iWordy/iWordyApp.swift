1f3640dccb696307c22b28a46ff27c2a06e8dac5
Orhan Özdemir <oozdemir@procsin.com>
2026-05-28 23:43:23 +0300

    Initial Commit

---
+++ iWordy/iWordyApp.swift
@@ -0,0 +1,17 @@
+//
+//  iWordyApp.swift
+//  iWordy
+//
+//  Created by Orhan Özdemir on 28.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct iWordyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
